adc263f7c4b85f626add4839cc14bcce685ba440
Serafín Piña <arespin23@gmail.com>
2019-05-19 22:49:12 +0200

    Minotaur move (better optimized)

--- Assets/Scripts/Enemies/Minotaur/Mino_Move.cs
+++ Assets/Scripts/Enemies/Minotaur/Mino_Move.cs
@@ -8,28 +8,42 @@ public class Mino_Move : MonoBehaviour
     private Rigidbody2D rb2d;
     int direction;//use to direction´s enemies
     [SerializeField] float speed;
+    [SerializeField]float speedMove;
+    
+    [SerializeField] float length;
+    float starPosition;
+    float counter;
+
+    float currentPosition;
+    float lastPosition;
 
     // Start is called before the first frame update
     void Start()
     {
+        speedMove = 0.8f;       
+        starPosition = transform.position.x;
         direction = 1;
         rb2d = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();//set component animator
-        speed = Mathf.Abs(rb2d.velocity.x);
+        anim.SetTrigger("go_walk");
     }
 
     void OnTriggerEnter2D(Collider2D col)
     {
         if (col.gameObject.tag == "Wall")
         {
+            //speedMove = 0f;
+            //Debug.Log("Wall" + speed);
             Flip();
         }
 
         if (col.tag == "Player")
         {
-            speed = 0;
+            speedMove = 0f;
+            anim.SetTrigger("finish_walk");
+            //speed = 0.4f;
             anim.SetTrigger("do_attack");
-            anim.ResetTrigger("finish_attack");
+            //anim.ResetTrigger("finish_attack");
             //Debug.Log("Muñecajo");
         }
 
@@ -38,17 +52,29 @@ public class Mino_Move : MonoBehaviour
     {
         if (col.tag == "Player")
         {
+            speedMove = 0.8f;
+            anim.SetTrigger("go_walk");
             //Debug.Log("Salgoooooo");
             anim.SetTrigger("finish_attack");
-            anim.ResetTrigger("do_attack");
+            //anim.ResetTrigger("do_attack");
+
         }
 
     }
 
     void Update()
     {
-        anim.SetFloat("Velocity", speed);
+        //anim.SetFloat("Velocity", speed);
         //Debug.Log( Mathf.Abs(rb2d.velocity.x));
+        counter += Time.deltaTime * speedMove;
+        transform.position = new Vector2(Mathf.PingPong(counter, length), transform.position.y);
+
+        currentPosition = transform.position.x;
+        Debug.Log(currentPosition);
+        if (currentPosition < lastPosition) transform.localScale = new Vector3(-1, 1, 1);
+        if (currentPosition > lastPosition) transform.localScale = new Vector3(1, 1, 1);
+        lastPosition = transform.position.x;
+
     }
 
     private void Flip()
@@ -60,8 +86,11 @@ public class Mino_Move : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-
-        if (direction == 1)
+        //Debug.Log(speed);
+        //transform.position = new Vector3(speed, myY, 0);
+        //Debug.Log("" + speed);
+           
+        /*if (direction == 1)
         {
             gameObject.GetComponent<SpriteRenderer>().flipX = false;
             transform.Translate(speed, 0, 0);
@@ -70,7 +99,7 @@ public class Mino_Move : MonoBehaviour
         {
             gameObject.GetComponent<SpriteRenderer>().flipX = true; //gira el sprite
             transform.Translate(-speed, 0, 0);
-        }
+        }*/
 
     }
 }
